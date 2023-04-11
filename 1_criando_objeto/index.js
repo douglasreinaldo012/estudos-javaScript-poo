fd2e91d1415ba89e5d3d380157d78e5bdbed6b92
Douglas Reinaldo <123218252+douglasreinaldo012@users.noreply.github.com>
2023-04-10 22:34:49 -0300

    v1

---
+++ 1_criando_objeto/index.js
@@ -0,0 +1,7 @@
+let carro = {nome:"celta",portas:4,freio:'abs'}
+console.log("   Carro    ")
+console.log("----------------")
+console.log(`Modelo:${carro.nome}`)
+console.log(`Nº de portas:${carro.portas}`)
+console.log(`Freios:${carro.freio}`)
+console.log("----------------")
