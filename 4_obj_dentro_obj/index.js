472ec5b76c8b1b40c58e650d1e172e84d5228343
Douglas Reinaldo <123218252+douglasreinaldo012@users.noreply.github.com>
2023-05-04 11:03:05 -0300

    exe javascript

---
+++ 4_obj_dentro_obj/index.js
@@ -0,0 +1,8 @@
+let pessoa = {
+    nome:"Rafael",
+    caracteristicas:{
+        olhos:"castanhos",cabelo:"preto",cor:"parda"
+    }
+}
+console.log("Nome:"+pessoa.nome)
+console.log("Olhos:"+pessoa.caracteristicas.olhos);
